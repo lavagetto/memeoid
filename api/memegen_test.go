558610342c6fe54db3b86b02b9d38f550f262fce
Di Stanislao Jacopo <jacopo.distanislao@gmail.com>
2021-03-27 11:49:45 +0100

    refactoring subtests in api/memegen_test.go

--- api/memegen_test.go
+++ api/memegen_test.go
@@ -72,72 +72,74 @@ func (s *MemeGenTestSuite) TestUID() {
 	s.NotEqual(uid, uid1, "expected the UIDs to be different for different capitalizations")
 }
 
-var testListGifs = []struct {
-	Path        string
-	ContentType string
-	Status      string
-	Body        string
-}{
-	{"", "application/json", "200 OK", `["badfile.gif","earth.gif"]`},
-	{"/nonexistent", "", "404 Not Found", ""},
-}
-
 func (s *MemeGenTestSuite) TestListGifs() {
-	reader := strings.NewReader("")
 	originalPath := s.Sut.ImgPath
-	for _, test := range testListGifs {
-		req := httptest.NewRequest(http.MethodGet, "http://localhost/gifs", reader)
-		req.Header.Set("Accept", "text/json")
-		rec := httptest.NewRecorder()
-		if test.Path != "" {
-			s.Sut.ImgPath = test.Path
-		} else {
-			s.Sut.ImgPath = originalPath
-		}
-		s.Sut.ListGifs(rec, req)
-		response := rec.Result()
-		if test.ContentType != "" {
-			s.Equal([]string{test.ContentType}, response.Header["Content-Type"])
-		}
-		if test.Body != "" {
-			defer response.Body.Close()
-			body, err := ioutil.ReadAll(response.Body)
-			s.Nil(err)
-			s.Equal(test.Body, string(body))
-		}
-		s.Equal(test.Status, response.Status)
+	var testCases = []struct {
+		Path        string
+		ContentType string
+		Status      string
+		Body        string
+	}{
+		{"", "application/json", "200 OK", `["badfile.gif","earth.gif"]`},
+		{"/nonexistent", "", "404 Not Found", ""},
+	}
+	for _, tc := range testCases {
+		testName := fmt.Sprintf("Path: %s - ContentType: %s - Status: %s - Body: %s", tc.Path, tc.ContentType, tc.Status, tc.Body)
+		s.Run(testName, func(){
+			req := httptest.NewRequest(http.MethodGet, "http://localhost/gifs", strings.NewReader(""))
+			req.Header.Set("Accept", "text/json")
+			rec := httptest.NewRecorder()
+			if tc.Path != "" {
+				s.Sut.ImgPath = tc.Path
+			} else {
+				s.Sut.ImgPath = originalPath
+			}
+			s.Sut.ListGifs(rec, req)
+			response := rec.Result()
+			if tc.ContentType != "" {
+				s.Equal([]string{tc.ContentType}, response.Header["Content-Type"])
+			}
+			if tc.Body != "" {
+				defer response.Body.Close()
+				body, err := ioutil.ReadAll(response.Body)
+				s.Nil(err)
+				s.Equal(tc.Body, string(body))
+			}
+			s.Equal(tc.Status, response.Status)
+		})
 	}
 }
 
-var testMemeGenerate = []struct {
-	Uri           string
-	StatusCode    int
-	FileGenerated bool
-}{
-	{"http://localhost/w/api.php", http.StatusBadRequest, false},
-	{"http://localhost/w/api.php?from=lala", http.StatusNotFound, false},
-	{"http://localhost/w/api.php?from=earth.gif", http.StatusBadRequest, false},
-	{"http://localhost/w/api.php?from=earth.gif&top=test", http.StatusPermanentRedirect, true},
-}
-
 func (s *MemeGenTestSuite) TestMemeGenerate() {
-	for _, test := range testMemeGenerate {
-
-		req := httptest.NewRequest(http.MethodGet, test.Uri, strings.NewReader(""))
-		rec := httptest.NewRecorder()
-
-		s.Sut.MemeFromRequest(rec, req)
-		response := rec.Result()
-
-		s.Equal(test.StatusCode, response.StatusCode)
-		
-		if test.FileGenerated {
-			uid, err := s.Sut.UID(req)
-			s.Nil(err)
-			filePath := path.Join(s.Sut.OutputPath, fmt.Sprintf("%s.gif", uid))
-			s.FileExists(filePath)
-			s.Contains(response.Header["Location"][0], fmt.Sprintf("/url/%s", uid))
-		}
+	var testCases = []struct {
+		Uri           string
+		StatusCode    int
+		FileGenerated bool
+	}{
+		{"http://localhost/w/api.php", http.StatusBadRequest, false},
+		{"http://localhost/w/api.php?from=lala", http.StatusNotFound, false},
+		{"http://localhost/w/api.php?from=earth.gif", http.StatusBadRequest, false},
+		{"http://localhost/w/api.php?from=earth.gif&top=test", http.StatusPermanentRedirect, true},
+	}
+	for _, tc := range testCases {
+		testName := fmt.Sprintf("Uri: %s - StatusCode: %d - Genereate: %t", tc.Uri, tc.StatusCode, tc.FileGenerated)
+		s.Run(testName, func() {
+			req := httptest.NewRequest(http.MethodGet, tc.Uri, strings.NewReader(""))
+			rec := httptest.NewRecorder()
+	
+			s.Sut.MemeFromRequest(rec, req)
+			response := rec.Result()
+	
+			s.Equal(tc.StatusCode, response.StatusCode)
+			
+			if tc.FileGenerated {
+				uid, err := s.Sut.UID(req)
+				s.Nil(err)
+				filePath := path.Join(s.Sut.OutputPath, fmt.Sprintf("%s.gif", uid))
+				s.FileExists(filePath)
+				s.Contains(response.Header["Location"][0], fmt.Sprintf("/url/%s", uid))
+			}
+		})
 	}
 }
 
